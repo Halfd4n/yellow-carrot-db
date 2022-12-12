10e48882c9e39d8262b84d40ea24c6cc130dba26
Benjamin Bonnerup <112319740+Halfd4n@users.noreply.github.com>
2022-12-12 12:53:45 +0100

    Finished Repos and UoW
    Completed Repositories and added Unit of Work to the Recipe- and IngredientRepository.

---
+++ Repositories/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YellowCarrotDb.Data;
+
+namespace YellowCarrotDb.Repositories;
+public class UnitOfWork
+{
+    private readonly RecipeDbContext _context;
+    private RecipeRepository _recipeRepository;
+    private IngredientRepository _ingredientRepository;
+
+    public UnitOfWork(RecipeDbContext context)
+    {
+        _context = context;
+    }
+
+    public RecipeRepository RecipeRepository
+    {
+        get
+        {
+            if(RecipeRepository is null)
+            {
+                _recipeRepository = new RecipeRepository(_context);
+            }
+
+            return _recipeRepository;
+        }
+    }
+
+    public IngredientRepository IngredientRepository
+    {
+        get
+        {
+            if(IngredientRepository is null)
+            {
+                _ingredientRepository = new IngredientRepository(_context);
+            }
+
+            return _ingredientRepository;
+        }
+    }
+
+    public async void SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,10 +1,52 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YellowCarrotDb.Data;
+using YellowCarrotDb.Models;
 
 namespace YellowCarrotDb.Repositories;
 public class UserRepository
 {
+    private readonly UserDbContext _context;
+
+    public UserRepository(UserDbContext context)
+	{
+        _context = context;
+    }
+
+    // Get all users from the database:
+    public async Task<List<User>> GetAllUsersAsync()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    // Get a specific user from the database using user id:
+    public async Task<User> GetUserByIdAsync(int userId)
+    {
+        return await _context.Users.FindAsync(userId);
+    }
+
+    // Get a specific user from the database using user name:
+    public async Task<User> GetUserByUserNameAsync(string userName)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.UserName.Equals(userName));
+    }
+
+    // Get a specific user from the database using user name and password:
+    public async Task<User> GetUserByUserNameAndPasswordAsync(string userName, string password)
+    {
+        return await _context.Users
+            .Where(u => u.UserName.Equals(userName) && u.Password.Equals(password))
+            .FirstOrDefaultAsync();
+    }
+
+    // Add a user to the database:
+    public async void AddUserAsync(User userToAdd)
+    {
+        await _context.Users.AddAsync(userToAdd);
+    }
 }
